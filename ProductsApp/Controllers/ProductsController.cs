fc62b1e7694b9f5005d5f8f41fb9f26456f8321b
blueboss500 <co.david.blue@gmail.com>
2015-09-06 13:38:17 -0600

    setup

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //member vars
+        private Product[] m_products = new Product[]
+        {
+            new Product{Id = 1, Name = "Tomato Paste", Category = "Groceries", Price = 1},
+            new Product{Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product{Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        };
+
+
+        //methods
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return m_products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = m_products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
