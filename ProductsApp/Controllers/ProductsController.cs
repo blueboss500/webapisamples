4d69244b73c9c1a76f3a6c736068d1906fbf43fa
blueboss500 <co.david.blue@gmail.com>
2015-09-06 13:53:27 -0600

    updated sample data

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -13,7 +13,7 @@ namespace ProductsApp.Controllers
         //member vars
         private Product[] m_products = new Product[]
         {
-            new Product{Id = 1, Name = "Tomato Paste", Category = "Groceries", Price = 1},
+            new Product{Id = 1, Name = "Tomato Paste Can", Category = "Groceries", Price = 1},
             new Product{Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
             new Product{Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
         };
